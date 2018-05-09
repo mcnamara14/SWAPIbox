a43138737c90eb5ce0f889b4fb7825860aef9553
Tyler McNamara <mcnamara14@gmail.com>
2018-05-09 16:28:55 -0600

    Add clean planet data functions in DataCleaner.js

--- src/Components/Stateless/Buttons/Buttons.js
+++ src/Components/Stateless/Buttons/Buttons.js
@@ -13,7 +13,7 @@ class Buttons extends Component {
     return (
       <section className="filterButtons">
         <button className="people" onClick={() => this.props.setData('people')} >people</button>
-        <button className="planets" onClick={() => dataCleaner.fetchData('planets')} >planets</button>
+        <button className="planets" onClick={() => this.props.setData('planets')} >planets</button>
         <button className="vehicles" onClick={() => dataCleaner.fetchData('vehicles')} >vehicles</button>
       </section>
     );
